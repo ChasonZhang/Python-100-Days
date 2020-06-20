2be14f700800c27a0ef50f89478f3eea4b8dbe54
zzz <zzz@MacBook-Pro.local>
2020-06-20 23:32:26 +0800

    Day-01

--- Day-01/day-01-import-turtle.py
+++ Day-01/day-01-import-turtle.py
@@ -1,17 +1,17 @@
 import turtle
 
-turtle.pensize(4)
-turtle.pencolor('red')
+# turtle.pensize(4)
+# turtle.pencolor('red')
 
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
 
-turtle.mainloop()
+# turtle.mainloop()
 
 # Output
 """
